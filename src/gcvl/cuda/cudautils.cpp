2bd560f5037265e75352d9a6c9f76272ac56e2b7
omar alvarez <xxosurfer3xx@gmail.com>
2015-07-14 23:46:43 +0200

    CUDAUtils CUDA_Array skeleton added

--- src/gcvl/cuda/cudautils.cpp
+++ src/gcvl/cuda/cudautils.cpp
@@ -24,80 +24,54 @@
 
 #include "cudautils.h"
 
-#include <iostream>
-#include <stdio.h>
-#include <assert.h>
-#include <cuda_runtime.h>
-//#include "helper_cuda.h"
+template <class T>
+CUDA_Array<T>::CUDA_Array()
+{
+    N                           = 0;
+    sizeof_element              = 0;
+    new_array_size_bytes        = 0;
+    host_array                  = NULL;
+}
 
-using namespace gcvl::cuda;
+template <class T>
+void CUDA_Array<T>::Initialize(int _N, const size_t _sizeof_element,
+                                 T *&_host_array)
+{
+    assert(_host_array != NULL);
 
-CudaUtils::CudaUtils() {
+    N               = _N;
+    sizeof_element  = _sizeof_element;
+    host_array      = _host_array;
+    new_array_size_bytes = N * sizeof_element;
 
-	std::cout << " **** Initializing CUDA Core ****" << std::endl;
+    //device_array = clCreateBuffer(context, flags, new_array_size_bytes, NULL, &err);
+    //OpenCL_Test_Success(err, "clCreateBuffer()");
+}
 
-	int deviceCount;
-	    cudaGetDeviceCount(&deviceCount);
-	    if (deviceCount == 0)
-	        printf("There is no device supporting CUDA\n");
-	    int dev;
-	    for (dev = 0; dev < deviceCount; ++dev) {
-	        cudaDeviceProp deviceProp;
-	        cudaGetDeviceProperties(&deviceProp, dev);
-	        if (dev == 0) {
-	            if (deviceProp.major == 9999 && deviceProp.minor == 9999)
-               printf("There is no device supporting CUDA.\n");
-	            else if (deviceCount == 1)
-	                printf("There is 1 device supporting CUDA\n");
-	            else
-	                printf("There are %d devices supporting CUDA\n", deviceCount);
-	        }
-	        printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);
-	        printf("  Major revision number:                         %d\n",
-	               deviceProp.major);
-	        printf("  Minor revision number:                         %d\n",
-	               deviceProp.minor);
-	        printf("  Total amount of global memory:                 %u bytes\n",
-	               deviceProp.totalGlobalMem);
-    #if CUDART_VERSION >= 2000
-	        printf("  Number of multiprocessors:                     %d\n",
-	               deviceProp.multiProcessorCount);
-	        printf("  Number of cores:                               %d\n",
-	               8 * deviceProp.multiProcessorCount);
-	    #endif
-	        printf("  Total amount of constant memory:               %u bytes\n",
-	               deviceProp.totalConstMem);
-	        printf("  Total amount of shared memory per block:       %u bytes\n",
-               deviceProp.sharedMemPerBlock);
-	        printf("  Total number of registers available per block: %d\n",
-	               deviceProp.regsPerBlock);
-	        printf("  Warp size:                                     %d\n",
-	               deviceProp.warpSize);
-	        printf("  Maximum number of threads per block:           %d\n",
-               deviceProp.maxThreadsPerBlock);
-	        printf("  Maximum sizes of each dimension of a block:    %d x %d x %d\n",
-	               deviceProp.maxThreadsDim[0],
-               deviceProp.maxThreadsDim[1],
-	               deviceProp.maxThreadsDim[2]);
-	        printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
-	               deviceProp.maxGridSize[0],
-	               deviceProp.maxGridSize[1],
-               deviceProp.maxGridSize[2]);
-	        printf("  Maximum memory pitch:                          %u bytes\n",
-	               deviceProp.memPitch);
-	        printf("  Texture alignment:                             %u bytes\n",
-	               deviceProp.textureAlignment);
-	        printf("  Clock rate:                                    %.2f GHz\n",
-	               deviceProp.clockRate * 1e-6f);
-    #if CUDART_VERSION >= 2000
-	        printf("  Concurrent copy and execution:                 %s\n",
-	               deviceProp.deviceOverlap ? "Yes" : "No");
-	    #endif
-    }
-	    printf("\nTest PASSED\n");
+template <class T>
+void CUDA_Array<T>::Release_Memory()
+{
+    //if (device_array)
+        //clReleaseMemObject(device_array);
+}
+
+template <class T>
+void CUDA_Array<T>::Host_to_Device()
+{
 
 }
 
-CudaUtils::~CudaUtils() {
-	std::cout << " **** Destroying CUDA Core ****" << std::endl;
+// *****************************************************************************
+template <class T>
+void CUDA_Array<T>::Device_to_Host()
+{
+
 }
+
+//TODO Add more types?
+template class CUDA_Array<float>;
+template class CUDA_Array<double>;
+template class CUDA_Array<int>;
+template class CUDA_Array<unsigned int>;
+template class CUDA_Array<char>;
+template class CUDA_Array<unsigned char>;
--- src/gcvl/cuda/cudautils.h
+++ src/gcvl/cuda/cudautils.h
@@ -24,20 +24,35 @@
 
 #pragma once
 
-#include "../export.h"
-#include "../gcvlconfig.h"
+template <class T>
+class CUDA_Array
+{
+private:
+    int N;                              // Number of elements in array
+    size_t sizeof_element;              // Size of each array elements
+    uint64_t new_array_size_bytes;      // Size (bytes) of new padded array
+    T     *host_array;                  // Pointer to start of host array, INCLUDIǸG padding
+    //std::string platform;               // OpenCL platform
+    //cl_context context;                 // OpenCL context
+    //cl_command_queue command_queue;     // OpenCL command queue
+    //cl_device_id device;                // OpenCL device
+    //cl_int err;                         // Error code
 
-namespace gcvl { namespace cuda {
+    // Allocated memory on device
+    //cl_mem device_array;                // Memory of device
+    //cl_mem cl_array_size_bit;
+    //cl_mem cl_sha512sum;
 
-    class GCVL_EXPORT CudaUtils {
+public:
+    CUDA_Array();
+    void Initialize(int _N, const size_t _sizeof_element,
+                    T *&host_array);
+    void Release_Memory();
+    void Host_to_Device();
+    void Device_to_Host();
+    //void Validate_Data();
 
-    public:
-        CudaUtils();
-        ~CudaUtils();
-
-    private:
-		//Etc.
-
-    };
-
-} }
+    //inline cl_mem * Get_Device_Array() { return &device_array; }
+    inline T *      Get_Host_Pointer() { return  host_array;   }
+    //void Set_as_Kernel_Argument(cl_kernel &kernel, const int order);
+};
