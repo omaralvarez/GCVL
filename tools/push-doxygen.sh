62deaa4fb85fa964ab7d6f4cbffe48cf9a9c78c5
omar alvarez <xxosurfer3xx@gmail.com>
2015-09-10 19:16:02 +0200

    Automatic documentation creation script finished

--- tools/push-doxygen.sh
+++ tools/push-doxygen.sh
@@ -2,24 +2,21 @@
 
 REPO_PATH=git@github.com:omaralvarez/GCVL.git
 HTML_PATH=doc/html
+CLONE_PATH=gh-pages
 COMMIT_USER="travis-ci"
 COMMIT_EMAIL="travis@travis-ci.org"
 CHANGESET=$(git rev-parse --verify HEAD)
 
 echo -e "Publishing doxygen...\n"
 
-rm -rf ${HTML_PATH}
 mkdir -p ${HTML_PATH}
-git clone -b gh-pages "${REPO_PATH}" --single-branch ${HTML_PATH}
-
-cd ${HTML_PATH}
-git rm -rf .
-cd -
+git clone -b gh-pages "${REPO_PATH}" --single-branch ${CLONE_PATH}
 
 doxygen Doxyfile
 
-cd ${HTML_PATH}
-git add .
+cd ${CLONE_PATH}
+cp -Rf ${HTML_PATH} ./doxygen
+git add -f .
 git config user.name "${COMMIT_USER}"
 git config user.email "${COMMIT_EMAIL}"
 git commit -m "Automated documentation build for changeset ${CHANGESET}."
